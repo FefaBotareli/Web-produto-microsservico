fa5d067e6453b79a09599bc842c3b54a26b12be0
Fe_Andrd <160950998+FefaBotareli@users.noreply.github.com>
2024-10-04 00:45:34 -0300

    Add files via upload

---
+++ we_produto_repository/IProdutoRepository.cs
@@ -0,0 +1,14 @@
+﻿using web_produto_domain;
+
+namespace we_produto_repository
+{
+    public interface IProdutoRepository
+    {
+        Task<IEnumerable<Produto>> ListarProdutos();
+
+        Task SalvarProduto(Produto produto);
+        Task AtualizarProduto(Produto produto);
+
+        Task RemoverProduto(int id);
+    }
+}
---
+++ we_produto_repository/ProdutoRepository.cs
@@ -0,0 +1,58 @@
+﻿using MySqlConnector;
+using web_produto_domain;
+using static we_produto_repository.ProdutoRepository;
+
+namespace we_produto_repository
+{
+    public class ProdutoRepository
+    {
+        public class ProdutoRepository : IProdutoRepository
+        {
+            private readonly MySqlConnection mySqlConnection;
+
+            public ProdutoRepository()
+            {
+                string connectionString = "Server=localhost; Database=sys;User=root;Password=123;";
+                mySqlConnection = new MySqlConnection(connectionString);
+            }
+
+            public async Task<IEnumerable<Produto>> ListarProdutos()
+            {
+                await mySqlConnection.OpenAsync();
+                string query = "select Id, Nome, Descricao from produtos;";
+                var usuarios = await mySqlConnection.QueryAsync<Produto>(query);
+                await mySqlConnection.CloseAsync();
+
+                return produtos;
+            }
+
+            public async Task SalvarProduto(Produto produto)
+            {
+                await mySqlConnection.OpenAsync();
+                string sql = @"insert into produto(nome, descricao) 
+                            values(@nome, @descricao);";
+                await mySqlConnection.ExecuteAsync(sql, produto);
+                await mySqlConnection.CloseAsync();
+            }
+
+            public async Task AtualizarProduto(Produto produto)
+            {
+                await mySqlConnection.OpenAsync();
+                string sql = @"update produtos 
+                            set Nome = @nome, 
+	                            Descricao = @descricao
+                            where Id = @id;";
+                await mySqlConnection.ExecuteAsync(sql, produto);
+                await mySqlConnection.CloseAsync();
+            }
+
+            public async Task RemoverProduto(int id)
+            {
+                await mySqlConnection.OpenAsync();
+                string sql = @"delete from produtos where Id = @id;";
+                await mySqlConnection.ExecuteAsync(sql, new { id });
+                await mySqlConnection.CloseAsync();
+            }
+        }
+    }
+}
